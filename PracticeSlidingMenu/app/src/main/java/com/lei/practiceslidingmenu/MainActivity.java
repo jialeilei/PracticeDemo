a912c5e8f5cc7386c3f6e985e1b53ca5954a3bb3
JiaLeiLei <public_lei@126.com>
2016-08-05 22:19:30 +0800

    add slidingMenu to pricticeMvp

--- PracticeSlidingMenu/app/src/main/java/com/lei/practiceslidingmenu/MainActivity.java
+++ PracticeSlidingMenu/app/src/main/java/com/lei/practiceslidingmenu/MainActivity.java
@@ -1,10 +1,16 @@
 package com.lei.practiceslidingmenu;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private TextView tvControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,29 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
     private void initView(){
+        tvControl=(TextView)findViewById(R.id.tvTest);
+        tvControl.setOnClickListener(this);
+       /* mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout.get*/
+
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvTest:
+
+                break;
+        }
+    }
+
+    /*@Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        //If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        returnsuper.onPrepareOptionsMenu(menu);
+    }*/
+
 }
